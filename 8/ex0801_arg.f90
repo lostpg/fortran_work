d1a768b41282d164bff2a74c9a89602bca144818
nemo <xavii.l@outlook.com>
2017-05-09 22:53:57 +0800

    make arg version up-to-date.

--- 8/ex0801_arg.f90
+++ 8/ex0801_arg.f90
@@ -1,17 +1,18 @@
 SUBROUTINE my_son(mat,n)
 ! Gauss elimination
 ! to change a matrix to an uppeeeeeeeeer triangular one.
-! zero_judge: when elements in a column that are under the upper triangle are all-zero,
+! zero_judge: when elements in a column that are under the upper triangle are
+! all-zero,
 ! jump to the next column.
 ! swap,l: when a diagonal element is zero, swap this raw with another 
 !         raw whose corresponding element not zero.
 ! swap_time: times doing the raw-swap. A swap means a minor sign multiplied to
 ! matrix.???? (maybe)
 IMPLICIT NONE
-INTEGER :: n, i, j, k, l, swap, all_zero, swap_time=0
+INTEGER :: n, i, j, l, swap, all_zero, swap_time=0
 REAL :: mat(n,n), c, zero_judge
 DO i=1, n-1
-  zero_judge = 0
+  zero_judge = 0.0
   DO all_zero=i+1,n
     zero_judge = zero_judge +  mat(all_zero,i)
   END DO
@@ -23,6 +24,7 @@ DO i=1, n-1
     END DO
     IF (l/=i) THEN
       swap_time = swap_time + 1
+!     Swap two raws.
       DO swap=1,n
         c = mat(i,swap)
         mat(i,swap) = mat(l,swap)
@@ -30,61 +32,49 @@ DO i=1, n-1
       END DO
     END IF
     c = mat(j,i) / mat(i,i)
-    DO k=i,n
-      mat(j,k) = mat(j,k) - c * mat(i,k)
-    END DO
+    mat(j,i:n) = mat(j,i:n) - c * mat(i,i:n)
   END DO
 END DO
 ! IF (MOD(swap_time, 2) == 1) mat = -mat
 END SUBROUTINE
 
-PROGRAM EX0801
+PROGRAM EX0801_ARG
 IMPLICIT NONE
-CHARACTER(LEN=128) :: arg
-INTEGER :: n
-INTEGER :: p, q, i
+INTEGER :: n = 0
+INTEGER :: p
 REAL, ALLOCATABLE :: mat(:,:), mat_input(:)
 REAL :: rand
+CHARACTER(LEN=128) :: arg
 
-! WRITE(*,'(A,/,A)') 'Input length of a matrix, 0 for a fixed sample of 4*4:'
+! Input length of a matrix, 0 for a fixed sample of 4*4'
 CALL GETARG(1,arg)
 READ(arg,'(I2)')n
 
 IF (n==0) THEN
   n = 4
-  ALLOCATE(mat(n,n))
   ALLOCATE(mat_input(n*n))
   mat_input = (/ 1,5,9,13,2,6,10,14,3,7,11,15,4,8,12,16 /)
-  GOTO 10
-END IF
-
-! generate a seed and some random number then multiply and floor them
-! use them as matrix element
-ALLOCATE(mat(n,n))
-ALLOCATE(mat_input(n*n))
-CALL RANDOM_SEED()
-DO i=1,n*n
-  CALL RANDOM_NUMBER(rand)
-  mat_input(i) = FLOOR(20 * rand)
-END DO
-GOTO 10
-
-10    mat = RESHAPE(mat_input,(/n,n/))
-DO q=1,n,1
-  DO p=1,n,1
-    WRITE(*,'(1X,F7.3)',advance='no') mat(q,p)
+ELSE
+  ! generate a seed and some random number then multiply and floor them
+  ! use them as matrix element
+  ALLOCATE(mat_input(n*n))
+  CALL RANDOM_SEED()
+  DO p=1,n*n
+    CALL RANDOM_NUMBER(rand)
+    mat_input(p) = FLOOR(20 * rand)
   END DO
-  WRITE(*,*) ""
+END IF
+ALLOCATE(mat(n,n))
+mat = RESHAPE(mat_input,(/n,n/))
+DO p=1,n
+  WRITE(*,'(20(1X,F8.3))') mat(p,1:n)
 END DO
-WRITE(*,'(A)') "Output"
 
 CALL my_son(mat,n)
 
-DO q=1,n,1
-  DO p=1,n,1
-    WRITE(*,'(1X,F8.3)',advance='no') mat(q,p)
-  END DO
-  WRITE(*,*) ""
+WRITE(*,'(A)') "Output"
+DO p=1,n
+  WRITE(*,'(20(1X,F8.3))') mat(p,1:n)
 END DO
 DEALLOCATE(mat)
 DEALLOCATE(mat_input)
